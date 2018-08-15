13f4974a0b43e70bb18a3448bb90d2bd6cf69e15
skreebydba <skreebydba@users.noreply.github.com>
2018-08-15 08:49:19 -0500

    Inital Commit
    Problem solved

---
+++ DistinctProducts.ps1
@@ -0,0 +1,6 @@
+﻿[bigint]$base = 2;
+[bigint]$exp = 100;
+[array]$products;
+[bigint]$product = [math]::pow($base,$exp);
+
+$product;
---
+++ fib1000.ps1
@@ -0,0 +1,24 @@
+﻿[bigint]$fib1 = 1;
+[bigint]$fib2 = 1;
+$thousand = 0;
+$i = 1;
+$fib = @($fib1, $fib2);
+
+$fib[0];
+$fib[1];
+
+while($thousand -eq 0)
+{
+    $fib += $fib[$i] + $fib[$i - 1];
+    
+    if($fib[$i].ToString().Length -eq 1000)
+    {
+        $fib[$i];
+        $i;
+        $thousand = 1;
+    }  
+
+    $i++;
+}
+
+
