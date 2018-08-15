17472e6ae6e1e846af73aae13e597ef3a2369c56
skreebydba <skreebydba@users.noreply.github.com>
2018-08-14 22:42:21 -0500

    Completed

--- FactorialDigitSum.ps1
+++ FactorialDigitSum.ps1
@@ -6,3 +6,16 @@ for($i = 2;$i -le 100;$i++)
 }
 
 $factorial;
+
+$factstr = $factorial.ToString();
+$factarr = $factstr.ToCharArray();
+$len = $factarr.Length;
+
+$total = [int]::Parse($factarr[0]) + [int]::Parse($factarr[1]);
+
+for($i = 2;$i -lt $len;$i++)
+{
+    $total = $total + [int]::Parse($factarr[$i]);
+}
+
+$total;
