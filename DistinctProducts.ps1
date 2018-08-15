073566c1a543e2c1cc32174bf09ac90f0556dcb4
skreebydba <skreebydba@users.noreply.github.com>
2018-08-15 09:12:37 -0500

    Completed problem

--- DistinctProducts.ps1
+++ DistinctProducts.ps1
@@ -1,6 +1,15 @@
-﻿[bigint]$base = 2;
-[bigint]$exp = 100;
-[array]$products;
-[bigint]$product = [math]::pow($base,$exp);
+﻿[bigint]$base = 0;
+[bigint]$exp = 0;
+[array]$products = @();
 
-$product;
+for($exp = 2;$exp -le 100;$exp = $exp + 1)
+{
+    for ($base = 2;$base -le 100;$base = $base + 1)
+    {
+        [bigint]$product = [math]::pow($base,$exp);
+        $products += $product;
+    }
+}
+
+$products = $products | Sort-Object | Get-Unique;
+$products.Length;
